8e434cbbf7cad473922d4b25d97ab7e48fd8eb20
Benji Lanyado <benjilanyado@gmail.com>
2013-03-14 15:27:57 +0000

    updates

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,60 +1,60 @@
 var places = [
     {
-        name: 'The Royal Oak',
+        name: 'The Griffin',
         selector: '#oak',
         closeSelector: '#close1',
-        backgroundHeight: '1258px',
+        backgroundHeight: '1256px',
         classesToRemove: 'rightalign botrightalign botleftalign',
         classesToAdd: 'leftalign',
-        embed: '<iframe frameborder="0" marginheight="0" marginwidth="0" style="border:  0;" src="http://static.guim.co.uk/interactivestore/2013/3/7/1362654594510/815048/publish_to_web/_files/iframe.html" width="550" height="400" allowfullscreen="false" mozallowfullscreen="false" webkitallowfullscreen="false" oallowfullscreen="false" msallowfullscreen="false"></iframe>',
-        stuffToShowOnClose: '#bottomleft, .pubname',
+        embed: '<iframe src="http://player.vimeo.com/video/61738339?title=0&amp;byline=0&amp;portrait=0" width="800" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+        stuffToShowOnClose: '#topright, .pubname',
         fadeDelay: 1000
     },
     {
-        name: 'The Griffin',
+        name: 'The Princess Royal',
         selector: '#griffin',
         closeSelector: '#close2',
         backgroundHeight: '1256px',
         classesToRemove: 'rightalign leftalign botrightalign',
         classesToAdd: 'botleftalign',
-        embed: "",
+        embed: '<iframe src="http://player.vimeo.com/video/61738344?title=0&amp;byline=0&amp;portrait=0" width="800" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
         stuffToShowOnClose: '.pubname, #thegame',
         fadeDelay: 1000
     },
 
     {
-        name: 'The Game',
+        name: 'The Game (click to start/stop)',
         selector: '#gamecopy',
         closeSelector: '#close3',
         backgroundHeight: '628px',
         classesToRemove: 'botleftalign leftalign botrightalign',
         classesToAdd: 'rightalign',
-        embed: "",
-        stuffToShowOnClose: '.pubname, #topright',
+        embed: '<iframe frameborder="0" marginheight="0" marginwidth="0" style="border:  0;" src="http://static.guim.co.uk/draft/interactivestore/2013/3/14/1363265282279/166737/publish_to_web/_files/iframe.html" width="750" height="500" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>',
+        stuffToShowOnClose: '.pubname, #bottomright',
         fadeDelay: 50
     },
 
     {
-        name: 'The New Inn',
+        name: 'The Royal Oak',
         selector: '#newinn',
         closeSelector: '#close4',
         backgroundHeight: '1256px',
         classesToRemove: 'botleftalign leftalign botrightalign',
         classesToAdd: 'rightalign',
-        embed: "",
-        stuffToShowOnClose:'#bottomright, .pubname',
+        embed: '<iframe src="http://player.vimeo.com/video/61738338?title=0&amp;byline=0&amp;portrait=0" width="800" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+        stuffToShowOnClose:'#thegame, .pubname',
         fadeDelay: 1000
     },
 
     {
-        name: 'The Princess Royal',
+        name: 'The New Inn',
         selector: '#princess',
         closeSelector: '#close5',
         backgroundHeight: '1256px',
         classesToRemove: 'botleftalign leftalign rightalign',
         classesToAdd: 'botrightalign',
-        embed: "",
-        stuffToShowOnClose:'.pubname',
+        embed: '<iframe src="http://player.vimeo.com/video/61738480?title=0&amp;byline=0&amp;portrait=0" width="800" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+        stuffToShowOnClose:'.pubname, #bottomleft',
         fadeDelay: 1000
     }
 ];
@@ -75,23 +75,23 @@ function processPlaceClick(place) {
     $('.pubname').fadeOut(place.fadeDelay);
 
     window.setTimeout(function(){
-        background.removeClass(place.classesToRemove).addClass(place.classesToAdd);
+        background.removeClass(place.classesToRemove).addClass(place.classesToAdd);       
         background.animate({'height': place.backgroundHeight}, place.fadeDelay, function(event){
             videoheadholder.fadeIn();
             videohead.html(place.name).fadeIn();
             videoholder.html(place.embed).fadeIn();
             videowrapper.fadeIn();
-            console.log("place open");
         });
     }, place.fadeDelay);
 
     var close = $(place.closeSelector);
     close.delay(2500).fadeIn(function(){
         close.addClass("open");
+        $("#videowrapper").addClass("open")
     });
 
+   
     close.on('click', function(event){
-        console.log("close");
         event.preventDefault();
         if (close.hasClass("open")) {
             videowrapper.fadeOut();
@@ -101,7 +101,7 @@ function processPlaceClick(place) {
             $(background).animate({'height': originalBackgroundHeight}, place.fadeDelay);
 
             $(this).fadeOut(function(){
-                $(".pubname").delay(place.fadeDelay).fadeIn(function(){
+                $(".pubname").delay(1000).fadeIn(function(){
                     $(place.stuffToShowOnClose).fadeIn();
                 });
             });
